ea4ca13ea88a92217fa23a2e06048aed0c79741e
Redevillain <redevillain@bk.ru>
2019-09-30 18:30:24 +0300

    Магия php почты

--- send.php
+++ send.php
@@ -2,7 +2,8 @@
 
 	
     $u_name=$_POST["name"];
-    $u_num=$_POST["number"]; 
+	$u_num=$_POST["number"]; 
+	$d = date_create()->format('Y-m-d H:i:s');
 
     if (isset($_POST["name"]) && isset($_POST["number"])){
 
@@ -22,10 +23,11 @@
 	# Проверка наличия записи
     $query ="INSERT INTO orders VALUES('$u_name','$u_num','$d')";
     $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link)); 
-	
-	
 	mysqli_close($link);
 	
 	}
+	$message = "Поступила заявка от человека по имени $u_name, с указанным номером: $u_num. Заявка поступила в $d.";
+	mail ("kotov.unispring@gmail.com","заявка",$message);
+
 	header('Location: index.html');
 ?>
